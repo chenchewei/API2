518be469b7b8432497c8cc25ee2882f961000f03
ChenCheWei <gunattack1120@gmail.com>
2020-05-28 22:53:02 +0800

    Station Table -(click and show address & Type to search stations)

--- API_2_0521/Station/StationViewController.swift
+++ API_2_0521/Station/StationViewController.swift
@@ -13,7 +13,7 @@ class StationViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet var StationTable: UITableView!
     @IBOutlet var StationText: UITextField!
     
-    var StationNameArr = ["Nangang","Taipei","Banciao","Taoyuan","Hsinchu","Miaoli","Taichung","Changhua","Yunlin","Chiayi","Tainan","Zuoying"]
+    var StationNameArr = ["南港","台北","板橋","桃園","新竹","苗栗","台中","彰化","雲林","嘉義","台南","左營"]
     var StationAddressArr = ["台北市南港區南港路一段313號","台北市北平西路3號","新北市板橋區縣民大道二段7號","桃園市中壢區高鐵北路一段6號","新竹縣竹北市高鐵七路6號","苗栗縣後龍鎮高鐵三路268號","台中市烏日區站區二路8號","彰化縣田中鎮站區路二段99號","雲林縣虎尾鎮站前東路301號","嘉義縣太保市高鐵西路168號","台南市歸仁區歸仁大道100號","高雄市左營區高鐵路105號"]
     
     override func viewDidLoad() {
@@ -21,16 +21,17 @@ class StationViewController: UIViewController, UITableViewDelegate, UITableViewD
         StationTable.register(UITableViewCell.self, forCellReuseIdentifier: "reuseCell")
         
     }
-    
+    /* Setup Station Table values */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 12
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = StationTable.dequeueReusableCell(withIdentifier: "reuseCell", for: indexPath)
-        cell.textLabel?.text = StationNameArr[indexPath.row]
-        
-        
-        return cell
+        var cell = StationTable.dequeueReusableCell(withIdentifier: "reuseCell")
+        cell = UITableViewCell.init(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "reuseCell")
+        cell?.textLabel?.text = StationNameArr[indexPath.row]
+        cell?.detailTextLabel?.text = StationAddressArr[indexPath.row]
+        return cell!
     }
+    
 }
