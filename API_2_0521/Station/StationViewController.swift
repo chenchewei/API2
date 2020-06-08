bc39272e1ff345023ff4c7700d6eed77550ec337
ChenCheWei <gunattack1120@gmail.com>
2020-06-08 23:04:49 +0800

    Alert view missing some functions. Station query almost done(Error: keyword is not name but address ->cell index error)

--- API_2_0521/Station/StationViewController.swift
+++ API_2_0521/Station/StationViewController.swift
@@ -8,19 +8,17 @@
 
 import UIKit
 
-class StationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {//,UISearchResultsUpdating
+class StationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet var StationTable: UITableView!
-    
+    /* Search bar settings*/
     var searchController: UISearchController!
     var resultName = [String]()
-    /*
-    var resultLocation: [String] = [String](){
-        didSet {
-            self.StationTable.reloadData(∫)
-        }
-        
-    }   //Search station location results*/
+    var resultAddress = [String]()
+    var shouldShowResult = false
+    /* Station datas*/
+    var Stations = ["南港":"台北市南港區南港路一段313號","台北":"台北市北平西路3號","板橋":"新北市板橋區縣民大道二段7號","桃園":"桃園市中壢區高鐵北路一段6號","新竹":"新竹縣竹北市高鐵七路6號","苗栗":"苗栗縣後龍鎮高鐵三路268號","台中":"台中市烏日區站區二路8號","彰化":"彰化縣田中鎮站區路二段99號","雲林":"雲林縣虎尾鎮站前東路301號","嘉義":"嘉義縣太保市高鐵西路168號","台南":"台南市歸仁區歸仁大道100號","左營":"高雄市左營區高鐵路105號"]
+    
     
     var StationNameArr = ["南港","台北","板橋","桃園","新竹","苗栗","台中","彰化","雲林","嘉義","台南","左營"]
     var StationAddressArr = ["台北市南港區南港路一段313號","台北市北平西路3號","新北市板橋區縣民大道二段7號","桃園市中壢區高鐵北路一段6號","新竹縣竹北市高鐵七路6號","苗栗縣後龍鎮高鐵三路268號","台中市烏日區站區二路8號","彰化縣田中鎮站區路二段99號","雲林縣虎尾鎮站前東路301號","嘉義縣太保市高鐵西路168號","台南市歸仁區歸仁大道100號","高雄市左營區高鐵路105號"]
@@ -33,80 +31,60 @@ class StationViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     override func viewWillAppear(_ animated: Bool) {
         searchController = UISearchController(searchResultsController: nil)
-        //searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
         searchController.searchBar.delegate = self
-        definesPresentationContext = true
-        //searchController.isActive = true
-        //print("Pre")
-        //print(searchController.isActive)
     }
+    /* Search and filter stations*/
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print("search2 working")
-        let searchText = searchController.searchBar.text!
-        print(searchController.isActive)
-        print(searchText)
-        resultName = StationNameArr.filter({ (Stations) -> Bool in
-            //print(resultName)
-            return Stations.contains(searchText)
-        })
-        print("Done?")
-        //StationTable.reloadData()
-        
-    }
-    /*
-    func updateSearchResults(for searchController: UISearchController) {
-        print("search working")
-        let searchText = searchController.searchBar.text!
-        resultName = StationNameArr.filter({ (Stations) -> Bool in
-            print(resultName)
-            return Stations.contains(searchText)
-        })
-        StationTable.reloadData()
-        print("Done")
-        
-        /*if let searchText = SearchController?.searchBar.text{
-            print("searching")
-            filterContent(for: searchText)
+        if(searchText == ""){
+            shouldShowResult = false
             StationTable.reloadData()
-        }*/
-    }
-    /*func filterContent(for searchText: String){
-        
-        resultName = StationNameArr.filter({(filterArr) -> Bool in
-            let words = filterArr
-            let Matched = words.localizedCaseInsensitiveContains(searchText)
-            print("filtering")
-            return Matched
+        }
+        else{
+            shouldShowResult = true
+        }
+        resultName = Stations.keys.filter({ (TempStations) -> Bool in
+            let StationTempText: String = TempStations
+            return (StationTempText.contains(searchText))
             
         })
-    }*/
-    */
-    
+        resultAddress = Stations.values.filter({ (TempAddress) -> Bool in
+            let AddressTempText: String = TempAddress
+            return (AddressTempText.contains(searchText))
+        })
+        print(resultName)
+        print(resultAddress)
+        StationTable.reloadData()
+
+    }
     
     /* Setup Station Table values */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(navigationItem.searchController?.isActive == true){
-            return resultName.count
+        //return 12
+        if(shouldShowResult == true){
+            if(resultAddress.count > resultName.count){
+                return resultAddress.count
+            }
+            else{
+                return resultName.count
+            }
         }
         else{
-            return StationNameArr.count
+            return Stations.keys.count
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = StationTable.dequeueReusableCell(withIdentifier: "reuseCell")
         cell = UITableViewCell.init(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "reuseCell")
-        if(navigationItem.searchController?.isActive == true){
+        if(shouldShowResult == true){
+                        
             cell?.textLabel?.text = resultName[indexPath.row]
+            cell?.detailTextLabel?.text = resultAddress[indexPath.row]
         }
-        else{/*
-            let resultN = ((searchController?.isActive)!) ? resultName[indexPath.row] : StationNameArr[indexPath.row]
-            let resultL = ((searchController?.isActive)!) ? resultLocation[indexPath.row] : StationAddressArr[indexPath.row]*/
-            
+        else{
             cell?.textLabel?.text = StationNameArr[indexPath.row]
             cell?.detailTextLabel?.text = StationAddressArr[indexPath.row]
-            //print("not active")
         }
         
         return cell!
