bbeaabc552638e447bfc12a71a7054536444b82e
ChenCheWei <gunattack1120@gmail.com>
2020-06-09 22:11:32 +0800

    Alert view missing some functions. Station query finished. Developing selected and jumped back function

--- API_2_0521/Station/StationViewController.swift
+++ API_2_0521/Station/StationViewController.swift
@@ -13,16 +13,14 @@ class StationViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet var StationTable: UITableView!
     /* Search bar settings*/
     var searchController: UISearchController!
-    var resultName = [String]()
     var resultAddress = [String]()
-    var shouldShowResult = false
-    /* Station datas*/
-    var Stations = ["南港":"台北市南港區南港路一段313號","台北":"台北市北平西路3號","板橋":"新北市板橋區縣民大道二段7號","桃園":"桃園市中壢區高鐵北路一段6號","新竹":"新竹縣竹北市高鐵七路6號","苗栗":"苗栗縣後龍鎮高鐵三路268號","台中":"台中市烏日區站區二路8號","彰化":"彰化縣田中鎮站區路二段99號","雲林":"雲林縣虎尾鎮站前東路301號","嘉義":"嘉義縣太保市高鐵西路168號","台南":"台南市歸仁區歸仁大道100號","左營":"高雄市左營區高鐵路105號"]
-    
-    
+    var shouldShowResult = false    // flag
+    /* Station datas */
+    var Stations = ["台北市南港區南港路一段313號":"南港","台北市北平西路3號":"台北","新北市板橋區縣民大道二段7號":"板橋","桃園市中壢區高鐵北路一段6號":"桃園","新竹縣竹北市高鐵七路6號":"新竹","苗栗縣後龍鎮高鐵三路268號":"苗栗","台中市烏日區站區二路8號":"台中","彰化縣田中鎮站區路二段99號":"彰化","雲林縣虎尾鎮站前東路301號":"雲林","嘉義縣太保市高鐵西路168號":"嘉義","台南市歸仁區歸仁大道100號":"台南","高雄市左營區高鐵路105號":"左營"]
+        /* For init data */
     var StationNameArr = ["南港","台北","板橋","桃園","新竹","苗栗","台中","彰化","雲林","嘉義","台南","左營"]
     var StationAddressArr = ["台北市南港區南港路一段313號","台北市北平西路3號","新北市板橋區縣民大道二段7號","桃園市中壢區高鐵北路一段6號","新竹縣竹北市高鐵七路6號","苗栗縣後龍鎮高鐵三路268號","台中市烏日區站區二路8號","彰化縣田中鎮站區路二段99號","雲林縣虎尾鎮站前東路301號","嘉義縣太保市高鐵西路168號","台南市歸仁區歸仁大道100號","高雄市左營區高鐵路105號"]
-    
+/* Pre load */
     override func viewDidLoad() {
         super.viewDidLoad()
         StationTable.register(UITableViewCell.self, forCellReuseIdentifier: "reuseCell")
@@ -34,6 +32,7 @@ class StationViewController: UIViewController, UITableViewDelegate, UITableViewD
         navigationItem.searchController = searchController
         searchController.searchBar.delegate = self
     }
+/* Main */
     /* Search and filter stations*/
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if(searchText == ""){
@@ -43,52 +42,39 @@ class StationViewController: UIViewController, UITableViewDelegate, UITableViewD
         else{
             shouldShowResult = true
         }
-        resultName = Stations.keys.filter({ (TempStations) -> Bool in
-            let StationTempText: String = TempStations
-            return (StationTempText.contains(searchText))
-            
-        })
-        resultAddress = Stations.values.filter({ (TempAddress) -> Bool in
+        resultAddress = Stations.keys.filter({ (TempAddress) -> Bool in
             let AddressTempText: String = TempAddress
             return (AddressTempText.contains(searchText))
         })
-        print(resultName)
-        print(resultAddress)
+        //print(resultName)
+        //print(resultAddress)
         StationTable.reloadData()
 
     }
-    
     /* Setup Station Table values */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return 12
         if(shouldShowResult == true){
-            if(resultAddress.count > resultName.count){
                 return resultAddress.count
-            }
-            else{
-                return resultName.count
-            }
         }
         else{
             return Stations.keys.count
         }
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = StationTable.dequeueReusableCell(withIdentifier: "reuseCell")
         cell = UITableViewCell.init(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "reuseCell")
-        if(shouldShowResult == true){
-                        
-            cell?.textLabel?.text = resultName[indexPath.row]
+        if(shouldShowResult == true){   //searching
+            cell?.textLabel?.text = Stations[resultAddress[indexPath.row]]
             cell?.detailTextLabel?.text = resultAddress[indexPath.row]
         }
-        else{
+        else{   // init ot blank
             cell?.textLabel?.text = StationNameArr[indexPath.row]
             cell?.detailTextLabel?.text = StationAddressArr[indexPath.row]
         }
-        
         return cell!
     }
+    /* Developing*/
     
     /* Selected and jumped back(ongoing)*/
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
