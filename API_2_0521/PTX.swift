afb75ef74db54174a74349c766c8a46c33cc7107
ChenCheWei <gunattack1120@gmail.com>
2020-06-18 22:35:35 +0800

    Map functions completed. Figuring out how to receive datas from closure

---
+++ API_2_0521/PTX.swift
@@ -0,0 +1,25 @@
+//
+//  PTX.swift
+//  API_2_0521
+//
+//  Created by mmslab-mini on 2020/6/18.
+//  Copyright © 2020 mmslab-mini. All rights reserved.
+//
+
+import Foundation
+
+/* API data structure */
+public struct PTX : Codable {
+    var StationID : String
+    var StationAddress : String
+    var StationName : StationNames?
+    var StationPosition : StationPositions?
+}
+public struct StationNames : Codable {
+    var Zh_tw : String?
+    var En : String?
+}
+public struct StationPositions : Codable {
+    var PositionLat : Double?
+    var PositionLon : Double?
+}
--- API_2_0521/ViewController.swift
+++ API_2_0521/ViewController.swift
@@ -12,7 +12,6 @@ import Toast
 import Foundation
 import CommonCrypto
 
-
 /* PTX Auth key from sample code */
 enum CryptoAlgorithm {
     case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
@@ -69,21 +68,6 @@ let signDate = "x-date: " + xdate;
 let base64HmacStr = signDate.hmac(algorithm: .SHA1, key: APP_KEY)
 let authorization:String = "hmac username=\""+APP_ID+"\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\""+base64HmacStr+"\""
 
-/* API data structure */
-public class PTX : Codable {
-    var StationID : String
-    var StationAddress : String
-    var StationName : StationNames?
-    var StationPosition : StationPositions?
-}
-public class StationNames : Codable {
-    var Zh_tw : String?
-    var En : String?
-}
-public class StationPositions : Codable {
-    var PositionLat : Double?
-    var PositionLon : Double?
-}
 /* API datas*/
 public class Station {
     var StationName = ""
@@ -98,49 +82,53 @@ class ViewController: UIViewController,MKMapViewDelegate {
     @IBOutlet var StartingPoint: UITextField!
     @IBOutlet var Destination: UITextField!
     @IBOutlet var mapView: MKMapView!
-    var StationList = [Station]()
+    
     var StationData = [PTX]()
-
+    var StationList = [Station]()
+    var SearchList = [Station]()    
+           
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getDataFromAPI()
+        mapView.delegate = self
+    }
+    
+    /* Environments set up */
     func getDataFromAPI(){
         let url = URL(string: APIUrl)
         var request = URLRequest(url: url!)
         request.setValue(xdate, forHTTPHeaderField: "x-date")
         request.setValue(authorization, forHTTPHeaderField: "Authorization")
         request.setValue("gzip", forHTTPHeaderField: "Accept-Encoding")
-        
-        URLSession.shared.dataTask(with: request){ data, response,error in
-            do{
-//                print(response)
-//                print()
-                let StationData = try JSONDecoder().decode([PTX].self, from: data!)
-                print(StationData)
-                self.StationData = StationData
-                for i in 0..<StationData.count{
-                    let station = Station()
-                    station.StationName = StationData.
+        let task = URLSession.shared.dataTask(with: request){ data, response,error in
+        do {
+            self.StationData = try JSONDecoder().decode([PTX].self, from: data!)
+//            self.StationList = StationList
+  
+            for i in 0..<self.StationData.count {
+                let station = Station()
+                station.StationID = self.StationData[i].StationID
+                station.StationName = self.StationData[i].StationName?.Zh_tw ?? ""
+                station.StationAddress = self.StationData[i].StationAddress
+                station.StationPositionLat = self.StationData[i].StationPosition?.PositionLat ?? 0.0
+                station.StationPositionLon = self.StationData[i].StationPosition?.PositionLon ?? 0.0
+                
+                self.StationList.append(station)
+                
+                /* Station pins */
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2DMake(self.StationList[i].StationPositionLat, self.StationList[i].StationPositionLon)
+                annotation.title = self.StationList[i].StationName
+                annotation.subtitle = self.StationList[i].StationAddress
+                self.mapView.addAnnotation(annotation)
                 }
+//            print(self.StationList[0].StationName)
             }
-            catch{
-                print(error)
+        catch {
+            print(error)
             }
-        }.resume()
-    }
-    
-    
-    /*
-    var StationNameArr = ["南港","台北","板橋","桃園","新竹","苗栗","台中","彰化","雲林","嘉義","台南","左營"]
-    var StationAddressArr = ["台北市南港區南港路一段313號","台北市北平西路3號","新北市板橋區縣民大道二段7號","桃園市中壢區高鐵北路一段6號","新竹縣竹北市高鐵七路6號","苗栗縣後龍鎮高鐵三路268號","台中市烏日區站區二路8號","彰化縣田中鎮站區路二段99號","雲林縣虎尾鎮站前東路301號","嘉義縣太保市高鐵西路168號","台南市歸仁區歸仁大道100號","高雄市左營區高鐵路105號"]
-    var StationCoordinateArr = [25.053188323974609,121.60706329345703,25.047670364379883,121.51698303222656,25.013870239257813,121.46459197998047,25.012861251831055,121.21472930908203,24.808441162109375,121.04026031494141,24.605447769165039,120.82527160644531,24.112483978271484,120.615966796875,23.874326705932617,120.57460784912109,23.736230850219727,120.41651153564453,23.459506988525391,120.32325744628906,22.925077438354492,120.28620147705078,22.68739128112793,120.30748748779297]*/
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        mapView.delegate = self
-        getDataFromAPI()
-        
-    }
-/*
-    override func viewWillAppear(_ animated: Bool) {
-        AllStationsAnnotation()
+        }
+        task.resume()
     }
     /* Show current location */
     static var location:CLLocationManager? = nil
@@ -163,25 +151,12 @@ class ViewController: UIViewController,MKMapViewDelegate {
             view.makeToast("Starting point and destination should be different.")
         }
     }
-    /* Station pins */
-    func AllStationsAnnotation(){
-        var count = 0
-        for i in stride(from: 0, to: 24, by: 2){
-            let annotation = MKPointAnnotation()
-            print(annotation)
-            annotation.coordinate = CLLocationCoordinate2DMake(StationCoordinateArr[i], StationCoordinateArr[i+1])
-            annotation.title = StationNameArr[count]
-            annotation.subtitle = StationAddressArr[count]
-            //print(count)
-            count+=1
-            mapView.addAnnotation(annotation)
-        }
-    }
     /* Tap and show alert view */
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView){
+        let ann = view.annotation?.title        
         let alertController = UIAlertController(title: "選擇動作", message: "", preferredStyle: .alert)
-        let StartAction = UIAlertAction(title: "設成起點", style: .default,handler:{ (action) in /*self.StartingPoint.text = self.StationNameArr[count]*/})
-        let DestAction = UIAlertAction(title: "設成終點", style: .default)
+        let StartAction = UIAlertAction(title: "設成起點", style: .default,handler:{ (action) in self.StartingPoint.text = ann!!})
+        let DestAction = UIAlertAction(title: "設成終點", style: .default,handler:{ (action) in self.Destination.text = ann!!})
         let RestAction = UIAlertAction(title: "附近餐廳", style: .default)
         let CancelAction = UIAlertAction(title: "取消", style: .cancel)
         alertController.addAction(StartAction)
@@ -202,10 +177,10 @@ class ViewController: UIViewController,MKMapViewDelegate {
             self.performSegue(withIdentifier: "TimeTableSegue",sender: self)
         }
     }
-    /* Segue back from StationViewController[unwind FAILED] */
-
-    */
+    /* Jump to StationViewController */
+    @IBAction func StationClicked(_ sender: Any) {
+//        let
+    }
+    
+    
 }
-
-
-
